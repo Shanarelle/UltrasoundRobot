a16b6fe165c3da55f4a00e231de4560be4fa6fc8
Shanarelle <cvl_reid@hotmail.com>
2014-03-25 11:38:48 +0000

    grapher to simply graph samples from files

---
+++ ultrasound_processing/grapher.py
@@ -0,0 +1,92 @@
+import sys
+import getopt
+import numpy
+import math
+import matplotlib.pyplot as plt
+
+"""
+Demo of a simple plot with a custom dashed line.
+
+A Line object's ``set_dashes`` method allows you to specify dashes with
+a series of on/off lengths (in points).
+"""
+
+def graph_results(y_values, title):  
+    #parameters for the look
+    #plt.autoscale(False,'y')
+    plt.xlabel('time')
+    plt.ylabel('amplitude')
+    plt.suptitle(title)
+    
+    #fig = plt.figure()
+    ax = plt.subplot(111)
+    
+    lineCount = 0
+    
+    for series in y_values:
+        x = numpy.arange(len(series))
+        line, = ax.plot(x, series, '--', linewidth=2, label=str(lineCount))
+        if lineCount > 6:
+            dashes = [10, 5, 20, 5] # 10 points on, 5 off, 100 on, 5 off
+            line.set_dashes(dashes) # means the later ones in a different style
+        lineCount = lineCount + 1
+    #plt.acorr(y_values)
+    
+    # Shrink current axis's height by 10% on the bottom
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 + box.height * 0.25,
+                     box.width, box.height * 0.8])
+
+    # Put a legend below current axis
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+              fancybox=True, shadow=True, ncol=2)
+    #plt.ylim(-1.5, 2.0)    #think should change y axis max&min
+
+    plt.show()
+   
+    
+    
+def main(argv):
+    matcherfile = ''
+    samplefile = ''
+    number = ''
+    test = False
+    try:
+        opts, args = getopt.getopt(argv,"s:n:")
+    except getopt.GetoptError:
+        print 'test.py -s <samplefile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-s"):
+            samplefile = arg
+        elif opt in ("-n"):
+            number = arg
+    return samplefile, number
+    
+    
+    
+
+values = [[]]
+valueIndex = -1
+
+
+samplesfile, number = main(sys.argv[1:])
+
+file = open(samplesfile)
+for line in file:
+    if line.isspace():
+        continue
+    if 'START' in line:
+        valueIndex = valueIndex + 1
+        if valueIndex != 0:
+            values.append([])
+    if ',' not in line:
+        continue
+    part = line.partition(',')
+    item = float(part[2])
+    values[valueIndex].append(item)
+file.close()
+
+print repr(values)
+
+graph_results(values, "Sequence Number " + number)
